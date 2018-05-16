3f735ee706d8647cf24d932e9c8b4606349e03c4
WorkUserName <36057510+WorkUserName@users.noreply.github.com>
2018-05-16 11:38:16 +0100

    Add files via upload
    Menu updates

--- app/src/main/java/com/example/customlist/MainActivity.java
+++ app/src/main/java/com/example/customlist/MainActivity.java
@@ -2,10 +2,7 @@ package com.example.customlist;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,9 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(navListener);
 
         imageDisplay = (Button)findViewById(R.id.btnImageDisplay);
 
@@ -36,29 +30,4 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(MenuItem item) {
-                    Fragment selectedFragment = null;
-
-//                    switch (item.getItemId()) {
-//                        case R.id.navigation_home:
-//                            selectedFragment = new HomePage();
-//                            break;
-//                        case R.id.navigation_maps:
-//                            selectedFragment = new Profile();
-//                            break;
-//                        case R.id.navigation_camera:
-//                            selectedFragment = new fitnessAssessment();
-//                            break;
-//                    }
-
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment).commit();
-
-                    return true;
-                }
-            };
 }
